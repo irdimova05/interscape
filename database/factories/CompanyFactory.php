90b891a6d72760de88c276ed8c89ce15f35eac94
Irina Dimova <irinadimova82@gmail.com>
2023-02-05 22:00:49 +0200

    [ch40] Create Company model, controller and factory

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company,
+            'description' => $this->faker->text,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'address' => $this->faker->address,
+            'website' => $this->faker->url,
+            'logo' => $this->faker->imageUrl(640, 480, 'business', true),
+        ];
+    }
+}

d64a53c9b9dcab2901dbeff3f6c5c22550a3997c
Irina Dimova <irinadimova82@gmail.com>
2023-02-05 21:59:06 +0200

    [ch39] Create ad model

---
+++ app/Models/Ad.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ad extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'company_id',
+        'employer_id',
+    ];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}

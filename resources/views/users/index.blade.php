4787440dd3e450c30403ee63ea6d9a489c26932c
Irina Dimova <irinadimova82@gmail.com>
2023-02-28 09:21:39 +0200

    [ch63] Fix the alignment of the create button and the search field

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,27 +1,36 @@
 <x-app-layout>
     <div class="py-8">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex mb-2">
-            <button class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                {{ __('Създай') }}
-            </button>
-            <div class="flex">
-                <x-input-label for="search" :value="__('Търси по име:')" />
-                <x-text-input id="search" class="" type="text" name="search" :value="old('search')"/>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-between">
+            <div>
+                <button class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    {{ __('Създай') }}
+                </button>
             </div>
-        </div>
-
-        @foreach($users as $user)
-        <div class="mb-4">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4 flex">
-                    <div class="grow">
-                        <h2 class="mb-3 text-3xl font-extrabold tracking-tight text-gray-900">{{ $user->name }}</h2>
-
+            <div class="items-stretch">
+                <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none ">
+                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+                        </svg>
                     </div>
-
+                    <input type="sea" id="search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5 " placeholder="Търси по име">
                 </div>
             </div>
         </div>
-        @endforeach
+    </div>
+
+    @foreach($users as $user)
+    <div class="mb-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4 flex">
+                <div class="grow">
+                    <h2 class="mb-3 text-3xl font-extrabold tracking-tight text-gray-900">{{ $user->name }}</h2>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    @endforeach
     </div>
 </x-app-layout>

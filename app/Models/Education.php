5ec8ab910f26ef3eee2e13740190c298f57b785f
Irina Dimova <irinadimova82@gmail.com>
2023-03-03 23:38:05 +0200

    [ch76] Create educations table

---
+++ app/Models/Education.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Education extends Model
+{
+    use HasFactory;
+
+    protected $table = 'educations'; // hardcoded because the in the doctrine library education is an uncountable word without a plural form
+
+    const BACHELOR = 'bachelor';
+    const MASTER = 'master';
+    const DOCTORANT = 'doctorant';
+    const DOCTOR = 'doctor';
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+}
---
+++ database/seeders/EducationSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Education;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EducationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $educations = [
+            [
+                'name' => 'Бакалавър',
+                'slug' => 'bachelor',
+            ],
+            [
+                'name' => 'Магистър',
+                'slug' => 'master',
+            ],
+            [
+                'name' => 'Докторант',
+                'slug' => 'doctorant',
+            ],
+            [
+                'name' => 'Доктор',
+                'slug' => 'doctor',
+            ],
+        ];
+
+        foreach ($educations as $education) {
+            Education::create($education);
+        }
+    }
+}

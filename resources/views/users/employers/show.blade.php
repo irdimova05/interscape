cbabc5992def743e823acc98d98d20ec08fd6ab5
Irina Dimova <irinadimova82@gmail.com>
2023-03-14 15:14:50 +0200

    [ch71] Show attributes of the employer and the company

--- resources/views/users/employers/show.blade.php
+++ resources/views/users/employers/show.blade.php
@@ -8,7 +8,7 @@
                         <p class="text-gray-400">Обяви</p>
                     </div>
                     <div>
-                        <p class="font-bold text-gray-700 text-xl">10</p>
+                        <p class="font-bold text-gray-700 text-xl">{{$user->employer->employeeRange->range}}</p>
                         <p class="text-gray-400">Служители</p>
                     </div>
                     <div>
@@ -25,8 +25,8 @@
                 <div class="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center"><button class="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"> Connect</button> <button class="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"> Message</button> </div>
             </div>
             <div class="mt-20 text-center border-b pb-12">
-                <h1 class="text-4xl font-medium text-gray-700">{{$user->name}}, <span class="font-light text-gray-500">27</span></h1>
-                <p class="font-light text-gray-600 mt-3">Bucharest, Romania</p>
+                <h1 class="text-4xl font-medium text-gray-700">{{$user->employer->name}}</h1>
+                <p class="font-light text-gray-600 mt-3">{{$user->email}}</p>
                 <p class="mt-8 text-gray-500">Solution Manager - Creative Tim Officer</p>
                 <p class="mt-2 text-gray-500">University of Computer Science</p>
             </div>

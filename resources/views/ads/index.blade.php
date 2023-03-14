0efa1d4601c9d25aec1976878cf4289265659f03
Irina Dimova <irinadimova82@gmail.com>
2023-03-14 15:56:30 +0200

    [ch101] Set the button to redirect to the create blade

--- resources/views/ads/index.blade.php
+++ resources/views/ads/index.blade.php
@@ -3,7 +3,7 @@
         @role('employer')
         <div class="flex justify-between mb-2 max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div>
-                <button class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                <button onclick="window.location='{{ route("ads.create") }}'" class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                     {{ __('Създай') }}
                 </button>
             </div>

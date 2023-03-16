fba79e0c3abb0a5fe0d9560630998a6bfcef64fa
Irina Dimova <irinadimova82@gmail.com>
2023-03-16 17:54:18 +0200

    [ch113] Create user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
     <div class="flex justify-between mb-8">
         <div>
-            <button class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+            <a href='{{ route("users.create") }}' class=" text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                 {{ __('Създай') }}
-            </button>
+            </a>
         </div>
         <div class="items-stretch">
             <div class="relative">

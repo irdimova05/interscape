862295e23018d74060672dea87ee2ec3f3af196d
Irina Dimova <irinadimova82@gmail.com>
2023-03-16 20:53:22 +0200

    [ch68] Show edit button only if the auth user is the creator of the ad

--- resources/views/ads/show.blade.php
+++ resources/views/ads/show.blade.php
@@ -22,7 +22,7 @@
                                     </div>
                                 </div>
                                 <div class="flex items-center">
-                                    @role('employer')
+                                    @if($ad->employer->user->id == Auth::user()->id)
                                     <button type="button" class="flex items-center text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
                                         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 mr-1" x-tooltip="tooltip">
                                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
@@ -56,7 +56,7 @@
                                         <span>Активирай</span>
                                     </button>
                                     @endif
-                                    @endrole
+                                    @endif
 
                                     @role('admin')
                                     <button type="button" class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 flex items-center">

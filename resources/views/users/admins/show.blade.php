f85a333981170f47ddea7135f6fe968118175fe5
Irina Dimova <irinadimova82@gmail.com>
2023-03-17 14:14:04 +0200

    [ch88] Admin blade - complete

--- resources/views/users/admins/show.blade.php
+++ resources/views/users/admins/show.blade.php
@@ -3,18 +3,6 @@
         <div class="p-8 bg-white shadow mt-24">
             <div class="grid grid-cols-1 md:grid-cols-3">
                 <div class="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
-                    <div>
-                        <p class="font-bold text-gray-700 text-xl">wadawd</p>
-                        <p class="text-gray-400">Курс</p>
-                    </div>
-                    <div>
-                        <p class="font-bold text-gray-700 text-xl">wdawd</p>
-                        <p class="text-gray-400">Успех</p>
-                    </div>
-                    <div>
-                        <p class="font-bold text-gray-700 text-xl">wadawd</p>
-                        <p class="text-gray-400">Обучение</p>
-                    </div>
                 </div>
                 <div class="relative">
                     <div class="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
@@ -51,13 +39,10 @@
                 </div>
             </div>
             <div class="mt-20 text-center border-b pb-12">
-                <h1 class="text-4xl font-medium text-gray-700">{{$user->name}}, <span class="font-light text-gray-500">27</span></h1>
+                <h1 class="text-4xl font-medium text-gray-700">{{$user->name}}</h1>
                 <p class="font-light text-gray-600 mt-3">{{$user->email}}</p>
-                <p class="mt-8 text-gray-500">wadwad</p>
-                <p class="mt-2 text-gray-500">awdawd</p>
             </div>
             <div class="mt-12 flex flex-col justify-center">
-                <p class="text-gray-600 text-center font-light lg:px-16">awdwad</p>
             </div>
         </div>
     </div>

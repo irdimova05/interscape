770d66c01832da270137307c3ae74474d0f05e16
Irina Dimova <irinadimova82@gmail.com>
2023-04-19 13:55:46 +0300

    [ch126] Register complete blade and decline button

--- resources/views/auth/components/update-profile-information-form.blade.php
+++ resources/views/auth/components/update-profile-information-form.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h3 class="text-lg font-medium text-gray-900 mb-2">
-            {{ __('За да можете да използвате платформата, е необходимо да довършите вашия профил') }}
+            {{ __('За да можете да използвате платформата, е необходимо да довършите вашия профил.') }}
         </h3>
         <h2 class="text-lg font-medium text-gray-900 mb-2">
             {{ __('Информация за профила') }}
@@ -14,11 +14,7 @@
         @endrole
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
-
-    {!! Form::open(['route'=>'profile.update'], ['class'=>"mt-6 space-y-6"] ) !!}
+    {!! Form::open(['route'=>'profile.update', 'method'=>'patch'], ['class'=>"mt-6 space-y-6"]) !!}
 
     <div class="grid grid-cols-2">
 
@@ -27,7 +23,7 @@
             <div class="justify-center items-center">
                 <div>
                     <x-input-label for="name" :value="__('Име:')" />
-                    <x-text-input id="name" name="name" type="text" class="mt-1 block w-full mb-3 " :value="old('name', null)" required autofocus autocomplete="name" />
+                    <x-text-input id="name" name="name" type="text" class="mt-1 block w-full mb-3 " :value=" old('name', null)" required autofocus autocomplete="name" />
                     <x-input-error class="mt-2" :messages="$errors->get('name')" />
                 </div>
 
@@ -78,10 +74,38 @@
             <x-input-label for="employeeRange" :value="__('Брой служители:')" />
             {!! Form::select('employeeRange', $employeeRanges, null, ['class' => "border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"]) !!}
 
-            <x-input-label for="description" :value="__('За фирмата:')" />
+            <x-input-label for="description" class="mt-4" :value="__('За фирмата:')" />
             {!! Form::textarea('description', null, ["rows"=>"4", "class"=>"block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-4 "]) !!}
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
             @endrole
+
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Смяна на парола') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600 mb-4">
+                {{ __('Уверете се, че акаунтът Ви използва дълга парола, която съдържа различни символи, за да сте защитени.') }}
+            </p>
+
+            <div>
+                <x-input-label for="current_password" class="m-1" :value="__('Настояща парола:')" />
+                <x-text-input id="current_password" name="current_password" type="password" class="p-2.5 block w-full" autocomplete="current-password" />
+                <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+            </div>
+
+            <div>
+                <x-input-label for="password" class="m-1" :value="__('Нова парола:')" />
+                <x-text-input id="password" name="password" type="password" class="p-2.5 block w-full" autocomplete="new-password" />
+                <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+            </div>
+
+            <div>
+                <x-input-label for="password_confirmation" class="m-1" :value="__('Повторете паролата:')" />
+                <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="p-2.5 block w-full" autocomplete="new-password" />
+                <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+            </div>
+
+
         </div>
 
         <div class="items-center">
@@ -139,12 +163,23 @@
                 @endrole
             </div>
         </div>
-        <div class="flex items-center gap-4">
-            <x-primary-button class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{{ __('Запази') }}</x-primary-button>
+        <div class="inline-flex">
+            <div class="flex items-center gap-4">
+                <x-primary-button class="text-white bg-gradient-to-r mt-3 from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{{ __('Запази') }}</x-primary-button>
 
-            @if (session('status') === 'profile-updated')
-            <p x-data=" { show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Вашият профил е обновен.') }}</p>
-            @endif
+                @if (session('status') === 'password-updated')
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Паролата е променена успешно.') }}</p>
+                @endif
+            </div>
+            {!! Form::close() !!}
+            <!-- Authentication -->
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-secondary-button onclick="event.preventDefault();
+                                                this.closest('form').submit();" class="text-white bg-gradient-to-r mt-3 from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" formnovalidate>
+                    {{ __('Отказ') }}
+                </x-secondary-button>
+            </form>
         </div>
-        {!! Form::close() !!}
 </section>

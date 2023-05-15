b18107c4bb984574cda6274bb9003ad9d1639d87
Irina Dimova <irinadimova82@gmail.com>
2023-05-15 19:37:02 +0300

    [ch15] Application logo color

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('ads.index') }}">
-                        <x-application-logo class="block h-9 w-auto " />
+                        <x-application-logo class="block h-9 w-auto fill-current " />
                     </a>
                 </div>
 

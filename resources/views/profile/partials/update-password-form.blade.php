ccebd217c3ad46a3b1a538582ac7821c1cdfe5a2
Irina Dimova <irinadimova82@gmail.com>
2023-06-25 17:34:03 +0300

    [ch234] Fix password edit form

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -9,27 +9,27 @@
         </p>
     </header>
 
-    {!! Form::open(['route'=>['profile.password.update'], 'method'=>'put'], ['class'=>"mt-6 space-y-6"] ) !!}
+    {!! Form::open(['route'=>['profile.password.update'], 'method'=>'put'] ) !!}
     <div>
-        <x-input-label for="current_password" :value="__('Настояща парола:')" />
+        <x-input-label class="mt-6 space-y-6" for="current_password" :value="__('Настояща парола:')" />
         <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
         <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
     </div>
 
     <div>
-        <x-input-label for="password" :value="__('Нова парола:')" />
+        <x-input-label class="mt-6 space-y-6" for="password" :value="__('Нова парола:')" />
         <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
         <x-input-error :messages="$errors->get('password')" class="mt-2" />
     </div>
 
     <div>
-        <x-input-label for="password_confirmation" :value="__('Повторете паролата:')" />
+        <x-input-label class="mt-6 space-y-6" for="password_confirmation" :value="__('Повторете паролата:')" />
         <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
         <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
     </div>
 
     <div class="flex items-center gap-4">
-        <x-primary-button class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{{ __('Запази') }}</x-primary-button>
+        <x-primary-button class="mt-6 space-y-6 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">{{ __('Запази') }}</x-primary-button>
 
         @if (session('status') === 'password-updated')
         <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600">{{ __('Паролата е променена успешно.') }}</p>
